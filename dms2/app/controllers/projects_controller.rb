21869d496e2669d600642613cd930d8f68002bc9
Pipat Waitayaworanart <st116392@ait.asia>
2015-03-19 20:50:55 +0700

    Make dropdown menu for signout. Before do anything, user need to ligin first.

--- dms2/app/controllers/projects_controller.rb
+++ dms2/app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :authenticate_user!
 
   # GET /projects
   # GET /projects.json
