5e33cf30188e8387d2f1a52cb9b41a836b6fb864
Pipat Waitayaworanart <st116392@ait.asia>
2015-03-26 15:46:42 +0700

    Chang var in active admin

--- dms2/app/admin/document_version.rb
+++ dms2/app/admin/document_version.rb
@@ -22,9 +22,23 @@ ActiveAdmin.register DocumentVersion do
     #column :created_by_id
     column :updated_by_id
     column :document_id_id
+    column :project_id
+    column :version
     actions
   end
 
-  permit_params :id, :document_name, :version_document_name, :file_location, :comment, :created_by_id, :created_at, :updated_by_id, :updated_at, :document_id_id
+  form do |f|
+    f.inputs "Document Version Details" do
+      f.input :document_name
+      f.input :updated_by_id
+      f.input :document_id_id
+      f.input :project_id
+      f.input :version
+    end
+    f.actions
+  end
+
+
+  permit_params :id, :document_name, :version_document_name, :file_location, :comment, :created_by_id, :created_at, :updated_by_id, :updated_at, :document_id_id, :project_id, :version
 
 end
