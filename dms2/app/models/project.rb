a1f8d9b2cb98b670d68de4d1fedae6f0c89fdea2
Pipat Waitayaworanart <coploftbas@gmail.com>
2015-04-02 23:06:34 +0700

    fix bug that other manager that has been assigned as team member can assign team member also

--- dms2/app/models/project.rb
+++ dms2/app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ActiveRecord::Base
 	def self.search(search)
 	  if search
 	  	Project.where("project_name iLIKE ?", "%#{search}%" )
-	    # Project.find(:all, :conditions => ['project_name LIKE ?', "%#{search}%"])
 	  else
 	  	Project.all
 	  end
